7167e9eed011009acbe00dee267046da485ad826
ShenPen Cheng <2189824750@qq.com>
2025-12-11 15:38:33 +0800

    fix：user类显示字符串问题

--- rpc-api/src/main/java/com/csp/rpcapi/User.java
+++ rpc-api/src/main/java/com/csp/rpcapi/User.java
@@ -3,12 +3,14 @@ package com.csp.rpcapi;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.io.Serializable;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@ToString
 public class User implements Serializable {
     String name;
     long id;
