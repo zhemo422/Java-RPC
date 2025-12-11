c725b99331b41d7dd67996400e890a351e8a14c5
ShenPen Cheng <2189824750@qq.com>
2025-12-11 15:33:18 +0800

    add:服务注册接口

--- rpc-core/src/main/java/com/csp/rpccore/transmission/RpcServer.java
+++ rpc-core/src/main/java/com/csp/rpccore/transmission/RpcServer.java
@@ -1,5 +1,10 @@
 package com.csp.rpccore.transmission;
 
+import com.csp.rpccore.dto.server.RpcServiceConfig;
+
 public interface RpcServer {
     void start();
+
+
+    public void publishService(RpcServiceConfig config);
 }
